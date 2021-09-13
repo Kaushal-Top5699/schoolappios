76181c518038f560bd83fde685cfa06b7ae1ade4
Kaushal <kaushaltop@me.com>
2021-09-13 14:02:48 +0530

    Initial Commit

---
+++ SchoolApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SchoolApp
+//
+//  Created by Kaushal Topinkatti B on 13/09/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
